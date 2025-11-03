58bf990079bca288871c43191e0b36c9c4e6d1df
TIENVUZ-tech <tien3152005@gmail.com>
2025-11-04 04:05:58 +0700

    Update ListSortable.java

--- ListSortable.java
+++ ListSortable.java
@@ -9,6 +9,17 @@ public class ListSortable<E extends Comparable<E>> {
     }
 
     public void print() {
+        int n = list.size();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (list.get(j).compareTo(list.get(j + 1)) > 0) {
+                    E temp = list.get(j);
+                    list.set(j, list.get(j + 1));
+                    list.set(j + 1, temp);  
+                }
+            }
+        }
+
         for (E thesis : list) {
             System.out.println(thesis.toString());
         }
